67764ec4573fae7c8ad173027e79545eb6e374a2
NotArdrey <neilardrey14@gmail.com>
2025-05-23 02:17:23 +0800

    service not complete

--- pages/header.php
+++ pages/header.php
@@ -243,7 +243,7 @@ if (in_array($_SESSION['role_id'], $officialRoles, true)) {
 <!-- Logout -->
 <li>
   <form id="logoutForm" action="../functions/logout.php" method="post">
-    <button type="button" id="logoutBtn" class="nav-link">
+    <button type="submit" id="logoutBtn" class="nav-link">
       <span class="icon-container logout">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
@@ -260,7 +260,8 @@ if (in_array($_SESSION['role_id'], $officialRoles, true)) {
   <main class="ml-64 p-8 space-y-8">
     <script>
    document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('logoutBtn').addEventListener('click', () => {
+  document.getElementById('logoutBtn').addEventListener('click', function(e) {
+    e.preventDefault();
     Swal.fire({
       title: 'Ready to leave?',
       text: "Select 'Logout' below if you are ready to end your current session.",
